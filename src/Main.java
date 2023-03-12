20d0a052c6845be54cdcaec27acfe3d745fd2fa9
EvgeniaVL <e9320915@yandex.ru>
2023-03-12 17:13:44 +0300

    change src/

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int price = 5700;
-        int x = 20;
-        int bonus = price / 20;
-
-        System.out.println(bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
